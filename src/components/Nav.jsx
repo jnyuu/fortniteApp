f17a50363eafad3418aac654f016f16165b3b47e
jnyuu <kozik.kuba@gmail.com>
2021-01-22 22:19:14 +0100

    fixed routing v3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,18 +6,24 @@ export default class Nav extends Component {
     return (
       <nav className="navbar navbar-expand navbar-dark bg-dark " id="mainNav">
         <div className="container">
-          <Link to="/" className="navbar-brand js-scroll-trigger">
+          <Link to="/fortniteApp" className="navbar-brand js-scroll-trigger">
             Main
           </Link>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link to="/about" className="nav-link js-scroll-trigger">
+                <Link
+                  to="/fortniteApp/about"
+                  className="nav-link js-scroll-trigger"
+                >
                   About
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/shop" className="nav-link js-scroll-trigger">
+                <Link
+                  to="/fortniteApp/shop"
+                  className="nav-link js-scroll-trigger"
+                >
                   Shop
                 </Link>
               </li>
